6de0dd30ebfb71287715f5511f503a97db346c10
aymather@gmail.com <aymather@gmail.com>
2018-11-20 14:12:46 -0600

    fjk

---
+++ main_app.js
@@ -0,0 +1,18 @@
+// Requires
+const scrape = require('./scrape.js');
+
+urls = [
+    'https://www.quora.com/profile/Nicolas-Cole-1/answers/published',
+    'https://www.quora.com/profile/Drew-Reggie-1/answers/published'
+];
+
+
+module.exports = function main_app() {
+	urls.forEach((url, index) => {
+
+	    setTimeout(() => {
+	        scrape(url);
+	    }, index*2000);
+
+	});
+}
